6e13e572ec1d0844eae224a7f29223ef0643f5c0
MRIT Software <contato.mrit@gmail.com>
2026-01-21 04:17:15 -0300

    fix: add missing ViewGroup import in AddProductActivity

--- app/src/main/java/com/bootreceiver/app/ui/AddProductActivity.kt
+++ app/src/main/java/com/bootreceiver/app/ui/AddProductActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
